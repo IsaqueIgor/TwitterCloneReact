70ede5a206ab91dedbd653760950098b43bdafc9
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-07-04 11:56:23 -0300

    Initial Commit

---
+++ src/components/Nav/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.scss';
+
+const Nav = () => {
+  return (
+    <div className='Nav-component'>
+      <div className='Nav-width'>
+        <div className='Nav'>
+          <div className='Nav-Content'>
+            <nav className='Nav-wrapper'></nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState, useContext, useRef } from 'react';
+import './styles';
+
+const Home = () => {
+  return (
+    <div className='Home-wrapper'>
+      <div className='Home-header-wrapper'>
+        <h2 className='Home-header'>Latest Tweets</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
